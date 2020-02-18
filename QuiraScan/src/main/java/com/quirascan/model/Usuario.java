f83d102be3d1821696260d2a454ec65bf417a4b2
Lenovo <Lenovo@DESKTOP-DFU5HIA>
2020-02-18 01:21:26 -0600

    dos

--- QuiraScan/src/main/java/com/quirascan/model/Usuario.java
+++ QuiraScan/src/main/java/com/quirascan/model/Usuario.java
@@ -3,6 +3,15 @@ package com.quirascan.model;
 public class Usuario {
 	
 	private int idUsuario;
+
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	
 	
 
 }
